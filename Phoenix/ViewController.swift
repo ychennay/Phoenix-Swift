dd56e829d29f17d2d356485cda9f1353b51b77da
Yu Chen <ychennay@gmail.com>
2017-07-02 21:56:40 -0700

    Initial Commit

---
+++ Phoenix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Phoenix
+//
+//  Created by Yu Chen on 7/2/17.
+//  Copyright © 2017 Yu Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
