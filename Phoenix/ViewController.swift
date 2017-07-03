8d55b6c8c9c3cd17c201b14a7b40ea33f39301f9
Yu Chen <ychennay@gmail.com>
2017-07-02 22:15:39 -0700

    initial

--- Phoenix/ViewController.swift
+++ Phoenix/ViewController.swift
@@ -7,12 +7,71 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet var loginButton: UIButton!
+    
+    @IBOutlet var textfield: UITextField!
+    
+    @IBOutlet var textLabel: UILabel!
+    
+    @IBAction func loginAction(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newValue = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newValue.setValue(textfield.text, forKey: "first_name" )
+        
+        do{
+            try context.save()
+            textfield.alpha = 0
+            loginButton.alpha = 0
+            textLabel.alpha = 1
+            textLabel.text = "Hi there, " + textfield.text! + "!"
+            
+        }catch{
+            print("Was unable to save.")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(request)
+            
+            for result in results as! [NSManagedObject]{
+                if let username = result.value(forKey:"first_name") as? String{
+                    
+                    textfield.alpha = 0
+                    loginButton.alpha = 0
+                    textLabel.alpha = 1
+                    textLabel.text = "Hi there, " + username + "!"
+                    
+                }
+            }
+            
+            
+        } catch {
+            
+        }
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +81,3 @@ class ViewController: UIViewController {
 
 
 }
-
